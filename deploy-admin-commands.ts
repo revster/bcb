e9a7913d84145bc49db9da7ac51416685e2e20a2
rreyv <rreyv@protonmail.com>
2026-04-18 12:57:18 -0500

    Restrict all commands in deploy-admin-commands to admin-only

--- deploy-admin-commands.ts
+++ deploy-admin-commands.ts
@@ -14,26 +14,19 @@
 import 'dotenv/config';
 import { REST, Routes, PermissionFlagsBits } from 'discord.js';
 
-const ADMIN_COMMANDS    = ['register', 'unregister', 'checkup', 'test'];
-const REPORT_COMMANDS   = ['stats', 'leaderboard', 'club-stats'];
+const ALL_COMMANDS = ['register', 'unregister', 'checkup', 'test', 'stats', 'leaderboard', 'club-stats'];
 
-const adminCommands = ADMIN_COMMANDS.map(name =>
-  require(`./commands/${name}`).data.toJSON()
-);
-
-const reportCommands = REPORT_COMMANDS.map(name => {
+const allCommands = ALL_COMMANDS.map(name => {
   const json = require(`./commands/${name}`).data.toJSON();
   json.default_member_permissions = PermissionFlagsBits.Administrator.toString();
   return json;
 });
 
-const allCommands = [...adminCommands, ...reportCommands];
-
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN as string);
 
 (async () => {
   try {
-    console.log(`Registering commands: ${[...ADMIN_COMMANDS, ...REPORT_COMMANDS].join(', ')}...`);
+    console.log(`Registering commands: ${ALL_COMMANDS.join(', ')}...`);
     await rest.put(
       Routes.applicationGuildCommands(process.env.CLIENT_ID as string, process.env.GUILD_ID as string),
       { body: allCommands }
